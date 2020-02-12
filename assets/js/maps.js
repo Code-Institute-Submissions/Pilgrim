0985630f0d9e176f793fb496ba436ae2c097fa0d
Luc Gotink <L.h.gotink@gmail.com>
2020-02-12 12:49:36 +0000

    gave Initmap array with coords, loop that loops through coords and check content function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -7,8 +7,25 @@ function initMap(){
     };
     // New map, targets the #map div
     var map = new google.maps.Map(document.getElementById('map'), options);
-
     
+    // Array of markers
+
+    var markers = [
+        {
+        coords:{lat:41.902782,lng:12.496366}
+        },
+        {
+        coords:{lat:42.878212,lng:-8.544844}
+        }
+    ];
+    
+    // Loop through markers
+    for(var i = 0; i <= markers.length; i++){
+        // Add marker
+        addMarker(markers[i]);
+    }
+
+
     function addMarker(props) {
         var marker = new google.maps.Marker({
             position: props.coords,
@@ -21,7 +38,18 @@ function initMap(){
             // set icon image
             marker.setIcon(props.iconImage)
         }
+        // Check content
+        if(props.content){
+            var infoWindow = new google.maps.InfoWindow({
+                content:props.content
+            });
+
+            marker.addListener('click', function(){
+                infoWindow.open(map, marker);
+            });
+        }
 
     } 
 
-}
+};
+
